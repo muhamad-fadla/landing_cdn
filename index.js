76f786fbdb3b99baded0c4f83f75cae82ebd6ab9
muhamad-fadla <yuratamma@gmail.com>
2021-10-10 12:29:09 +0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ app.set('cache', true)
 app.disable('x-powered-by')
 
 app.use((req,res,next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
+	 res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
 	next()
 })
 
